28812e2a27fcc8ceb44856e40af0163cd1a314e1
Lucas Viera <lucasviera22@gmail.com>
2024-11-04 16:16:04 -0300

    api and db code

---
+++ app/db.py
@@ -0,0 +1,24 @@
+import os
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# DB setup
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# DB Model
+class Patient(Base):
+    __tablename__ = "patients"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    phone = Column(String(20), nullable=False)
+    document_photo = Column(String(255), nullable=False)
+
+Base.metadata.create_all(bind=engine)
---
+++ app/main.py
@@ -0,0 +1,64 @@
+import os
+import shutil
+from fastapi import FastAPI, HTTPException, File, Form, UploadFile, BackgroundTasks
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from dotenv import load_dotenv
+from app.db import Patient, engine, SessionLocal, Base
+load_dotenv()
+app = FastAPI()
+
+
+
+@app.get("/")
+def read_root():
+    return {"message": "API is working OK"}
+
+
+# model for patient
+class PatientRegistration(BaseModel):
+    name: str
+    email: EmailStr
+    phone: str
+
+# endpoint for patient registration
+@app.post("/register")
+async def register_patient(
+    name: str = Form(...),
+    email: EmailStr = Form(...),
+    phone: str = Form(...),
+    document_photo: UploadFile = File(...),
+    background_tasks: BackgroundTasks = BackgroundTasks()
+):
+    try:
+        log_request(name, email, phone, document_photo.filename)
+        # input validation
+        patient_data = PatientRegistration(name=name, email=email, phone=phone)
+
+        # photo file save
+        photo_path = f"uploads/{document_photo.filename}"
+        with open(photo_path, "wb") as buffer:
+            shutil.copyfileobj(document_photo.file, buffer)
+
+        # save database
+        db = SessionLocal()
+        patient = Patient(
+            name=patient_data.name,
+            email=patient_data.email,
+            phone=patient_data.phone,
+            document_photo=photo_path,
+        )
+        db.add(patient)
+        db.commit()
+        db.refresh(patient)
+        db.close()
+        # send email async
+        return {"message": "Patient successfully registered"}
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        raise HTTPException(status_code=422, detail=str(e))
+
+
+def log_request(name, email, phone, file):
+    print(f"Received data: name={name}, email={email}, phone={phone}, file={file}")
